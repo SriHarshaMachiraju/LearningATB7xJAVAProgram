7021d9217ce70c0a7167fdf0728129335f9ee682
Sriharsha <sriharsha.9893@gmail.com>
2024-09-24 10:49:31 +0530

    updated date

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,7 +1,9 @@
 package ex_07072024;
-
+//Interview question: 	What is the output of this code
 public class Lab001 {
-    public static void main(String[] args) {
+    public static void main(String[] sriharsha) {
         System.out.println("Hello world");
     }
 }
+
+//There will be no difference
---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,8 @@
+package ex_07072024;
+//Interview question: Can I run a class without a main method?
+public class Lab003 {
+    public static void apple(String[] args) {
+        //Keywords and Identifier
+    }
+}
+//Ans: no
